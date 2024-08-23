28a3853953c8bb0f4c550277ec2de82240f48fde
Sugui <anaisusg@gmail.com>
2024-08-23 09:01:04 +0200

    Disabled the possibility of ProductStub to have a 0 in its id field

--- src/test/java/com/shopping/api/stub/ProductStubBuilder.java
+++ src/test/java/com/shopping/api/stub/ProductStubBuilder.java
@@ -6,13 +6,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import com.shopping.api.model.Product;
 
-public class ProductStub {
+public class ProductStubBuilder {
 
     private static Random random = new Random();
 
     public static Product.ProductBuilder builder() {
         return Product.builder()
-                .id(random.nextLong(Long.MAX_VALUE))
+                .id(random.nextLong(Long.MAX_VALUE - 1) + 1)
                 .description(RandomStringUtils.randomAlphabetic(0, 100))
                 .amount(random.nextInt(10));
     }
