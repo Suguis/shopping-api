930f0d3e00400a2aecf884e2b86d0e7d884b035a
Sugui <anaisusg@gmail.com>
2024-08-26 12:22:55 +0200

    Fixed product stub generation

--- src/test/java/com/shopping/api/stub/ProductStubBuilder.java
+++ src/test/java/com/shopping/api/stub/ProductStubBuilder.java
@@ -6,9 +6,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import com.shopping.api.model.Product;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class ProductStubBuilder {
 
     public static Product.ProductBuilder builder() {
@@ -17,6 +14,6 @@ public class ProductStubBuilder {
         return Product.builder()
                 .id(id)
                 .description(RandomStringUtils.randomAlphabetic(0, 100))
-                .amount(random.nextInt(10));
+                .amount(random.nextInt(9) + 1);
     }
 }
